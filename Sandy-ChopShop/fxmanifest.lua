2169fdff85d1a29fa492dff903d2135153439003
Kody Thompson <88422603+McNastty6699@users.noreply.github.com>
2022-11-06 19:42:54 -0600

    Add files via upload

---
+++ Sandy-ChopShop/fxmanifest.lua
@@ -0,0 +1,12 @@
+name "Chop $hop"
+author "McNastty6699"
+description "SANDY CHOPSHOP V1.0"
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+server_scripts {
+	'server.lua',
+}
---
+++ Sandy-ChopShop/server.lua
@@ -0,0 +1,3 @@
+print('\n\n- - - - - - - - - - - - - - - - - - - - - -\n^7[^$ANDY $HORE$^7] ^7[^CUSTOM MAPP CREATED BY MCNASTTY#1922^7] ^CHOP $HOP.^0\n- - - - - - - - - - - - - - - - - - - - - -\n')
+           
+      
